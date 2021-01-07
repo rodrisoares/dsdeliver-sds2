486dc1d1284d202c832ddc59e49934796fdc553e
unknown <diihhsoares2@gmail.com>
2021-01-07 14:17:40 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,29 @@
+import './styles.css';
+import { ReactComponent as YouTubeIcon } from './youtube.svg';
+import { ReactComponent as LinkedinIcon } from './linkedin.svg';
+import { ReactComponent as InstagramIcon } from './instagram.svg';
+
+function Footer() {
+    return (
+        <footer className="main-footer"> 
+        Site desenvolvido por Rodrigo Soares durante o evento DevSuperior
+
+        <div className="footer-icons">
+            <a href ="https://www.youtube.com/" target="_new">
+             <YouTubeIcon />
+            </a>
+            <a href ="https://br.linkedin.com/" target="_new">
+            <LinkedinIcon />
+            </a>
+            <a href ="https://www.instagram.com/" target="_new">
+            <InstagramIcon />
+            </a>
+
+        </div>
+        </footer>
+        
+    )
+}
+
+export default Footer;
+
