02fdbcec67a0a87cff634ebbfa89d3b6cd671a3d
unknown <diihhsoares2@gmail.com>
2021-01-22 10:04:17 -0300

    Add orders list

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, Text, View , Image } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+
 
  function Header() {
-  return (
-    <View style={styles.container}>
-      <Image source={require('../assets/logo.png')} />
-      <Text style={styles.text}> DS Delivery </Text>
+  const navigation = useNavigation();
 
-      
-    </View>
+  const handleOnPress = () => {
+    navigation.navigate('Home');
+  }
+
+  return (
+    <TouchableWithoutFeedback onPress={handleOnPress}> 
+     <View style={styles.container}>
+       <Image source={require('../assets/logo.png')} />
+       <Text style={styles.text}> DS Delivery </Text>
+     </View>
+    </TouchableWithoutFeedback>
   );
 }
 
--- front-mobile/src/Home/index.tsx
+++ front-mobile/src/Home/index.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, Text, View , Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header';
 
  function Home() {
+   const navigation = useNavigation();
 
-  const handleOnPress =() => {
 
+  const handleOnPress = () => {
+    navigation.navigate('Orders');
   }
-  
+
   return (
    <>
+     <Header />
      <View style ={styles.container}>
       <Image source={require('../assets/deliveryman.png')} /> 
-      <Text style={styles.title}>Acompanhe os pedidos e {'\n'}entregue no prazo!</Text>
-      <Text style={styles.subTitle}>Receba todos os pedidos do seu {'\n'}restaurante na palma da sua mão</Text>
+      <Text style={styles.title}>
+        Acompanhe os pedidos e {'\n'}entregue no prazo!
+        </Text>
+      <Text style={styles.subTitle}>
+        Receba todos os pedidos do seu {'\n'}restaurante na palma da sua mão
+        </Text>
      </View>
    
     <View style={styles.footer}>
-    <RectButton style={styles.button} onPress={}>
+    <RectButton style={styles.button} onPress={handleOnPress}>
         <Text style={styles.buttonText}> VER PEDIDOS</Text>
     </RectButton>
 
